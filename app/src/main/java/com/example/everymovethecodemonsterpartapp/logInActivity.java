5d2c2d8cb1c403bd041a1193c0718e320b79c0c4
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-05-06 12:46:37 +0530

    create the another activity call logInActivity

---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/logInActivity.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class logInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
