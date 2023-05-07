67a0d0585f0d9ef9fecdfd8828473edb15a65304
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-05-07 13:07:18 +0530

    add the DailyDataInput.java

---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/DailyDataInput.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DailyDataInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_data_input);
+    }
+}
