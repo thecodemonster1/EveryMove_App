2203cdd19918516daf0be5e620e8779fdaff2abd
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-05-07 13:58:51 +0530

    added a daily date picker and configured

--- app/src/main/java/com/example/everymovethecodemonsterpartapp/DailyDataInput.java
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/DailyDataInput.java
@@ -2,13 +2,46 @@ package com.example.everymovethecodemonsterpartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.DatePicker;
 
 public class DailyDataInput extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_data_input);
+
+        //set title of the app
+        getSupportActionBar().setTitle("Every Move");
+
+
+        datePickerDialog = (DatePickerDialog) findViewById(R.id.dailyDatePicker);
+    }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        datePickerDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                // Create an Intent to start the new activity
+                Intent intent = new Intent(getApplicationContext(), NewActivity.class);
+
+                // Pass the selected date to the new activity as an extra
+                String selectedDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                intent.putExtra("selected_date", selectedDate);
+
+                // Start the new activity
+                startActivity(intent);
+            }
+        });
+
     }
 }
