36115b728422bfbe4490e5048646f77443ec19d0
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-05-07 13:59:10 +0530

    created test today activity

---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/TodayTestAct.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodayTestAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_test);
+    }
+}
