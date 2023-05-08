ebf9dd8567057653b0b7ce4cf37a25cb5dd9eba7
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-05-08 11:37:03 +0530

    configuring the floating button

--- app/src/main/java/com/example/everymovethecodemonsterpartapp/TodayTestAct.java
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/TodayTestAct.java
@@ -3,18 +3,31 @@ package com.example.everymovethecodemonsterpartapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TodayTestAct extends AppCompatActivity {
-    private FloatingActionButton fb;
+
+    FloatingActionButton floatingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today_test);
 
+        floatingButton = findViewById(R.id.fadd);
+    }
 
+    @Override
+    public void onResume(){
+        super.onResume();
 
+        floatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
