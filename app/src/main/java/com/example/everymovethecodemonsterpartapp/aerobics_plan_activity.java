638eca41bbd25313e9e7ec458b8ac5ee8c6edb96
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-06-07 15:58:17 +0530

    created another activity call aerobics_plan_activity

---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/aerobics_plan_activity.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class aerobics_plan_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aerobics_plan);
+    }
+}
