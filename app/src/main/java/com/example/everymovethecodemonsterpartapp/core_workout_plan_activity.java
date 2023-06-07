d8c547b7291eb0afe6e2f452acbb7ca7d985d1ef
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-06-07 17:22:11 +0530

    some changes and created new workout plan activities...

---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/core_workout_plan_activity.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class core_workout_plan_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_core_workout_plan);
+    }
+}
---
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/strength_plan_activity.java
@@ -0,0 +1,14 @@
+package com.example.everymovethecodemonsterpartapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class strength_plan_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strength_plan);
+    }
+}
