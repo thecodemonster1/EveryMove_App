84c9cd528733b489b9bdfeb0fc21fa187f3f047e
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-06-07 13:32:17 +0530

    organize the codes by functions, score counting is working

--- app/src/main/java/com/example/everymovethecodemonsterpartapp/MainActivity.java
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/MainActivity.java
@@ -21,8 +21,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView message;
     private String uParent;
     private String uChild;
-    private String searching;
-    private String workoutPlan;
+    private String searchPlan;
 
 
     @Override
@@ -31,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference databaseReference = firebaseDatabase.getReference("Personal Plans").child("testID");
+        DatabaseReference databaseReference = firebaseDatabase.getReference("Personal Plans").child("testID3");
 
 
         //set title of the app
@@ -44,36 +43,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 //        // Perform the search query
-//        databaseReference.orderByChild(uParent).equalTo("BeginnersPlan").addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.orderByChild(uParent).equalTo(searchPlan).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Data is retrieved successfully
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    // Process each retrieved data item
+                    String data = snapshot.getValue(String.class);
+                    // Perform actions with the retrieved data
+                    // ...
+                    Toast.makeText(MainActivity.this, data, Toast.LENGTH_LONG).show();
+                    Intent iBig = new Intent(getApplicationContext(), beginners_plan_activity.class);
+                    startActivity(iBig);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Error occurred while retrieving data
+                // Handle the error
+                Toast.makeText(MainActivity.this, "Error in Database", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+//        inputBtn.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                // Data is retrieved successfully
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    // Process each retrieved data item
-//                    String data = snapshot.getValue(String.class);
-//                    // Perform actions with the retrieved data
-//                    // ...
-//
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                // Error occurred while retrieving data
-//                // Handle the error
-//                Toast.makeText(MainActivity.this, "Error in Database", Toast.LENGTH_LONG).show();
+//            public void onClick(View v) {
+//                Intent iBig = new Intent(getApplicationContext(), beginners_plan_activity.class);
+//                startActivity(iBig);
 //            }
 //        });
 
-
-        inputBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), beginners_plan_activity.class);
-                startActivity(i);
-            }
-        });
 
 
         // Attach a ValueEventListener to retrieve data
@@ -82,19 +84,23 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called when the data is changed or fetched for the first time
                 // Retrieve the data from the snapshot
+
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     // Access individual child nodes
                     String key = snapshot.getKey();
                     String value = snapshot.getValue(String.class);
 
+                    // Do something with the data
+
                     Toast.makeText(MainActivity.this, key +" : "+ value, Toast.LENGTH_LONG).show();
                     if (key.equals("planName")) {
                         // Use the value associated with the key
                         message.setText("Your Workout Plan is : " + value);
+                        searchPlan = value;
                         Toast.makeText(MainActivity.this, key +" : "+ value, Toast.LENGTH_LONG).show();
                         break; // Exit the loop if the key is found
                     }
-                    // Do something with the data
+
 
 
                     Log.d("FirebaseData", "Value: " + value);
@@ -104,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError error) {
                 // This method is called if the retrieval is canceled or fails
+                Toast.makeText(MainActivity.this, "Failed to retrieve data: " + error.getMessage(), Toast.LENGTH_LONG).show();
                 Log.e("FirebaseData", "Failed to retrieve data: " + error.getMessage());
             }
         });
--- app/src/main/java/com/example/everymovethecodemonsterpartapp/beginners_plan_activity.java
+++ app/src/main/java/com/example/everymovethecodemonsterpartapp/beginners_plan_activity.java
@@ -2,6 +2,8 @@ package com.example.everymovethecodemonsterpartapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,15 +12,19 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 
 public class beginners_plan_activity extends AppCompatActivity {
-    CheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, checkBox7;
+
     EditText Count1, Count2, Count3, Count4, Count5, Count6, Count7;
+    CheckBox checkBox1, checkBox2, checkBox3, checkBox4, checkBox5, checkBox6, checkBox7;
     Button increment1, increment2, increment3, increment4, increment5, increment6, increment7;
     Button decrement1, decrement2, decrement3, decrement4, decrement5, decrement6, decrement7;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginners_plan2);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(beginners_plan_activity.this);
 
 
         // 1 =============================================================
@@ -29,42 +35,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment1 = (Button) findViewById(R.id.increment1);
         decrement1 = (Button) findViewById(R.id.decrement1);
 
-        Count1.setText("0");
-
-        increment1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count1.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count1.setText(pCountStr);
-            }
-        });
-
-        decrement1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count1.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count1.setText(pCountStr);
-            }
-        });
-        checkBox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count1.setText("10");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count1.setText("0");
-                }
-            }
-        });
-
+        workoutMod(checkBox1, Count1, increment1, decrement1,"10");
 
         // 2 =============================================================
 
@@ -74,43 +45,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment2 = (Button) findViewById(R.id.increment2);
         decrement2 = (Button) findViewById(R.id.decrement2);
 
-        Count2.setText("0");
-
-        increment2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count2.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count2.setText(pCountStr);
-            }
-        });
-
-        decrement2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count2.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count2.setText(pCountStr);
-            }
-        });
-        checkBox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count2.setText("30");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count2.setText("0");
-                }
-            }
-        });
-
-
+        workoutMod(checkBox2, Count2, increment2, decrement2, "30");
 
         // 3 =============================================================
 
@@ -120,42 +55,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment3 = (Button) findViewById(R.id.increment3);
         decrement3 = (Button) findViewById(R.id.decrement3);
 
-        Count3.setText("0");
-
-        increment3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count3.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count3.setText(pCountStr);
-            }
-        });
-
-        decrement3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count3.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count3.setText(pCountStr);
-            }
-        });
-        checkBox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count3.setText("5");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count3.setText("0");
-                }
-            }
-        });
-
+        workoutMod(checkBox3, Count3, increment3, decrement3, "5");
 
         // 4 =============================================================
 
@@ -166,43 +66,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment4 = (Button) findViewById(R.id.increment4);
         decrement4 = (Button) findViewById(R.id.decrement4);
 
-        Count4.setText("0");
-
-        increment4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count4.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count4.setText(pCountStr);
-            }
-        });
-
-        decrement4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count4.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count4.setText(pCountStr);
-            }
-        });
-        checkBox4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count4.setText("5");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count4.setText("0");
-                }
-            }
-        });
-
-
+        workoutMod(checkBox4, Count4, increment4, decrement4, "5");
 
         // 5 =============================================================
 
@@ -213,42 +77,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment5 = (Button) findViewById(R.id.increment5);
         decrement5 = (Button) findViewById(R.id.decrement5);
 
-        Count5.setText("0");
-
-        increment5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count5.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count5.setText(pCountStr);
-            }
-        });
-
-        decrement5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count5.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count5.setText(pCountStr);
-            }
-        });
-        checkBox5.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count5.setText("10");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count5.setText("0");
-                }
-            }
-        });
-
+        workoutMod(checkBox5, Count5, increment5, decrement5, "10");
 
         // 6 =============================================================
 
@@ -258,43 +87,7 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment6 = (Button) findViewById(R.id.increment6);
         decrement6 = (Button) findViewById(R.id.decrement6);
 
-        Count6.setText("0");
-
-        increment6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count6.getText().toString().trim());
-                pCountInt++;
-                String pCountStr = Integer.toString(pCountInt);
-                Count6.setText(pCountStr);
-            }
-        });
-
-        decrement6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count6.getText().toString().trim());
-                pCountInt--;
-                String pCountStr = Integer.toString(pCountInt);
-                Count6.setText(pCountStr);
-            }
-        });
-        checkBox6.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Checkbox is checked
-                    // Perform your logic here when the checkbox is clicked
-                    Count6.setText("500");
-                } else {
-                    // Checkbox is not checked
-                    // Perform your logic here when the checkbox is not clicked
-                    Count6.setText("0");
-                }
-            }
-        });
-
-
+        workoutMod(checkBox6, Count6, increment6, decrement6, "500");
 
         // 7 =============================================================
 
@@ -304,43 +97,117 @@ public class beginners_plan_activity extends AppCompatActivity {
         increment7 = (Button) findViewById(R.id.increment7);
         decrement7 = (Button) findViewById(R.id.decrement7);
 
-        Count7.setText("0");
+        workoutMod(checkBox7, Count7, increment7, decrement7, "2");
 
-        increment7.setOnClickListener(new View.OnClickListener() {
+
+// Score counting =============================================================
+
+        Button addButton = (Button) findViewById(R.id.buttonAdd_plan_A);
+
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count7.getText().toString().trim());
+
+//                int total = ((int) (((((Integer.parseInt(Count1.getText().toString())/10.0)*100.0)+((Integer.parseInt(Count2.getText().toString())/30.0)*100.0)+((Integer.parseInt(Count3.getText().toString())/5.0)*100.0)+((Integer.parseInt(Count4.getText().toString())/5.0)*100.0)+((Integer.parseInt(Count5.getText().toString())/10.0)*100.0)+((Integer.parseInt(Count6.getText().toString())/500.0)*100.0)+((Integer.parseInt(Count7.getText().toString())/2.0)*100.0))/7)*0.05));
+                int CountInt1 = Integer.parseInt(Count1.getText().toString());
+                int CountInt2 = Integer.parseInt(Count2.getText().toString());
+                int CountInt3 = Integer.parseInt(Count3.getText().toString());
+                int CountInt4 = Integer.parseInt(Count4.getText().toString());
+                int CountInt5 = Integer.parseInt(Count5.getText().toString());
+                int CountInt6 = Integer.parseInt(Count6.getText().toString());
+                int CountInt7 = Integer.parseInt(Count7.getText().toString());
+
+                double score1 = (Integer.parseInt(Count1.getText().toString())/10.0)*100.0;
+                double score2 = (Integer.parseInt(Count2.getText().toString())/30.0)*100.0;
+                double score3 = (Integer.parseInt(Count3.getText().toString())/5.0)*100.0;
+                double score4 = (Integer.parseInt(Count4.getText().toString())/5.0)*100.0;
+                double score5 = (Integer.parseInt(Count5.getText().toString())/10.0)*100.0;
+                double score6 = (Integer.parseInt(Count6.getText().toString())/500.0)*100.0;
+                double score7 = (Integer.parseInt(Count7.getText().toString())/2.0)*100.0;
+
+                int totalScore = (int) (((score1 + score2 + score3 + score4 + score5 + score6 + score7) / 7)*0.05);
+
+                String scoreStr = Double.toString(totalScore);
+
+                builder.setTitle("Scores");
+                builder.setMessage("Your Total Score is "+ scoreStr);
+
+                // Set a positive button and its click listener
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Perform the desired action when the OK button is clicked
+                        // This can be left empty if you just want to dismiss the dialog
+                    }
+                });
+                builder.show();
+            }
+        });
+
+    }
+
+    // FUNCTIONS =============================================================
+// check minus value function =============================================================
+    public void checkInvalidValue(int pCountInt, EditText Count){
+        if (pCountInt < 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(beginners_plan_activity.this);
+            builder.setTitle("Error");
+            builder.setMessage("Invalid workout count!!!");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // Perform the desired action when the OK button is clicked
+                    // This can be left empty if you just want to dismiss the dialog
+                    Count.setText("0");
+                }
+            });
+            builder.show();
+
+        }
+    }
+
+
+    public void workoutMod(CheckBox checkBox, EditText count, Button increment, Button decrement,String target){
+
+        count.setText("0");
+
+        increment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int pCountInt = Integer.parseInt(count.getText().toString().trim());
                 pCountInt++;
                 String pCountStr = Integer.toString(pCountInt);
-                Count7.setText(pCountStr);
+                count.setText(pCountStr);
+                checkBox.setChecked(false);
             }
         });
 
-        decrement7.setOnClickListener(new View.OnClickListener() {
+        decrement.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int pCountInt = Integer.parseInt(Count7.getText().toString().trim());
+                int pCountInt = Integer.parseInt(count.getText().toString().trim());
                 pCountInt--;
                 String pCountStr = Integer.toString(pCountInt);
-                Count7.setText(pCountStr);
+                count.setText(pCountStr);
+                checkBox.setChecked(false);
+                checkInvalidValue(pCountInt, count);
             }
         });
-        checkBox7.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     // Checkbox is checked
                     // Perform your logic here when the checkbox is clicked
-                    Count7.setText("2");
+                    count.setText(target);
                 } else {
                     // Checkbox is not checked
                     // Perform your logic here when the checkbox is not clicked
-                    Count7.setText("0");
+//                    count.setText("0");
                 }
             }
         });
-
-
-
     }
 }
